c3f546b7f31c863151743fd06dcf8ff03a92e8c6
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-24 09:55:39 -0400

    Add project files.

---
+++ AverageCostBootcamp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AverageCostBootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var DaysOfCamp = 65;
+            var CostOfCamp = 15500m;
+            var CostPerDay = CostOfCamp/DaysOfCamp;
+            Console.WriteLine($"The cost of this bootcamp per day is ${CostPerDay}");
+        }
+    }
+}
