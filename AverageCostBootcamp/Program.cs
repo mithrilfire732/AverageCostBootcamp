ef3eca605fc93c15c7a87d74e0811a70eee416eb
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-24 12:06:37 -0400

    made message a variable

--- AverageCostBootcamp/Program.cs
+++ AverageCostBootcamp/Program.cs
@@ -8,8 +8,11 @@ namespace AverageCostBootcamp
         {
             var DaysOfCamp = 65;
             var CostOfCamp = 15500m;
-            var CostPerDay = CostOfCamp/DaysOfCamp;
-            Console.WriteLine($"The cost of this bootcamp per day is ${CostPerDay}");
+            var CostPerDay = CostOfCamp / DaysOfCamp;
+            var PreMessage = "The cost of this bootcamp per day is $";
+            var Message = $"{ PreMessage} {CostPerDay}";
+            Console.WriteLine($"{Message}");
+            System.Diagnostics.Debug.WriteLine($"{Message}");
         }
     }
 }
